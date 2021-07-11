2132762518a3366b5994c673264cd00adeb1d679
jonoman55 <jonoman55@yahoo.com>
2021-07-10 23:27:06 -0400

    added a comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HomeCard, Header, ApiCredit, Footer } from './index';
 
-// TODO : Add stying to here
+// TODO : Add styling to here
 const Home = (props) => {
     return (
         <>
